616eddadf6615becdf34e6ed61aae08cbc6ee63c
Meshack Bwire <b.meshack@outlook.com>
2024-07-08 09:33:37 +0300

    Home page done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Main.css';
 import emailIcon from './images/email_icon.png';
 import instagramIcon from './images/instagram_icon.png';
 import tiktokIcon from './images/tiktok_icon.png';
 
 const Main = () => {
-  const [activeDropdown, setActiveDropdown] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
@@ -14,7 +13,6 @@ const Main = () => {
   const handleClickOutside = (event) => {
     if (menuRef.current && !menuRef.current.contains(event.target)) {
       setIsMenuOpen(false);
-      setActiveDropdown(null);
     }
   };
 
@@ -25,67 +23,53 @@ const Main = () => {
     };
   }, []);
 
-  const handleLinkClick = () => {
-    setIsMenuOpen(false);
-    setActiveDropdown(null);
-  };
-
-  const handleDropdownClick = (dropdownName) => {
-    if (activeDropdown === dropdownName) {
-      setActiveDropdown(null);
-    } else {
-      setActiveDropdown(dropdownName);
-    }
-  };
-
   return (
     <div className="homepage">
       <div className="main-container">
+        <div className="logo">Infinity Wanderlust</div>
         <div className="nav-icon" onClick={toggleMenu}>
           &#9776; {/* Unicode character for hamburger icon */}
         </div>
-        <div className="logo">Infinity Wanderlust</div>
-        <ul ref={menuRef} className={`menu ${isMenuOpen ? 'active' : ''}`}>
-          <li className="item">
-            <div className="nav-link" onClick={() => handleDropdownClick('discover')}>
-              Discover
+        <div ref={menuRef} className={`menu ${isMenuOpen ? 'active' : ''}`}>
+          <div className="dropdown">
+            <button className="dropbtn">Discover</button>
+            <div className="dropdown-content">
+              <a href="#!">Travel Experiences</a>
+              <a href="#!">Places to Visit</a>
             </div>
-            {activeDropdown === 'discover' && (
-              <ul className="dropdown">
-                <li className="dropdown-item" onClick={handleLinkClick}>Travel Experiences</li>
-                <li className="dropdown-item" onClick={handleLinkClick}>Places to Visit</li>
-              </ul>
-            )}
-          </li>
-          <li className="item">
-            <div className="nav-link" onClick={() => handleDropdownClick('planTrip')}>
-              Plan Trip
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Plan Trip</button>
+            <div className="dropdown-content">
+              <a href="#!">Itineraries</a>
+              <a href="#!">Travel Tips</a>
             </div>
-          </li>
-          <li className="item">
-            <div className="nav-link" onClick={() => handleDropdownClick('review')}>
-              Review
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Review</button>
+            <div className="dropdown-content">
+              <a href="#!">Destination Reviews</a>
+              <a href="#!">Travel Blog</a>
             </div>
-          </li>
-          <li className="item">
-            <div className="nav-link" onClick={() => handleDropdownClick('needHelp')}>
-              Need Help?
-            </div>
-          </li>
-        </ul>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">About</button>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Contact</button>
+          </div>
+        </div>
       </div>
 
       <div className="bio-section">
-        <h2 style={{ fontSize: '43px', textAlign: 'center', marginBottom: '20px' }}>Explore.Dream.Discover</h2>
-        <p style={{ textAlign: 'center', fontFamily: 'Open Sans', fontSize: '18px', marginBottom: '20px' }}>
-          Hello! I'm a travel enthusiast who loves exploring new places and cultures. I believe that travel is not just about visiting new places but about experiencing life in different ways.
+        <h2>Explore.Dream.Discover</h2>
+        <p>Hello! I'm a travel enthusiast who loves exploring new places and cultures. I believe that travel is not just about visiting new places but about experiencing life in different ways.</p>
+        <p>
+          <a href="mailto:infinitywanderlusttravels@gmail.com"><img src={emailIcon} alt="Email" /></a>
+          <a href="https://www.instagram.com/infinity_wanderlust/" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram" /></a>
+          <a href="https://www.tiktok.com/@infinity_wanderlust" target="_blank" rel="noopener noreferrer"><img src={tiktokIcon} alt="TikTok" /></a>
         </p>
-        <p style={{ textAlign: 'center', marginBottom: '20px' }}>
-        <a href="mailto:infinitywanderlusttravels@gmail.com"><img src={emailIcon} alt="Email" /></a>
-        <a href="https://www.instagram.com/infinity_wanderlust/" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram" /></a>
-        <a href="https://www.tiktok.com/@infinity_wanderlust" target="_blank" rel="noopener noreferrer"><img src={tiktokIcon} alt="TikTok" /></a>
-        </p>
-        <p style={{ textAlign: 'center', marginTop: '20px' }}>
+        <p>
           <a href="#explore-more" className="explore-more-link">Start Exploring</a>
         </p>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,18 @@ import './Home.css';
 import Main from '../components/Main';
 
 const Home = () => {
-  const handleSearch = (location) => {
-    console.log(`Search for location: ${location}`);
+  // const handleSearch = (location) => {
+  //   console.log(`Search for location: ${location}`);
     
-  };
+  // };
 
   return (
-    <div>
+    <>
       <div className='homepage'>
       <Main />
       </div>
       
-    </div>
+    </>
   );
 };
 
