a3c96c9895eb5a845af0309d899b3d9d1bb2afd9
Meshack Bwire <b.meshack@outlook.com>
2024-07-08 10:36:47 +0300

    Navigation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,66 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
+// Main.js
+import React from 'react';
 import './Main.css';
+import Navigation from './Navigation';
 import emailIcon from './images/email_icon.png';
 import instagramIcon from './images/instagram_icon.png';
 import tiktokIcon from './images/tiktok_icon.png';
 
 const Main = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef(null);
-
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
-  const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setIsMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   return (
     <div className="homepage">
-      <div className="main-container">
-        <div className="logo">Infinity Wanderlust</div>
-        <div className="nav-icon" onClick={toggleMenu}>
-          &#9776; {/* Unicode character for hamburger icon */}
-        </div>
-        <div ref={menuRef} className={`menu ${isMenuOpen ? 'active' : ''}`}>
-          <div className="dropdown">
-            <button className="dropbtn">Discover</button>
-            <div className="dropdown-content">
-              <a href="#!">Travel Experiences</a>
-              <a href="#!">Places to Visit</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Plan Trip</button>
-            <div className="dropdown-content">
-              <a href="#!">Itineraries</a>
-              <a href="#!">Travel Tips</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Review</button>
-            <div className="dropdown-content">
-              <a href="#!">Destination Reviews</a>
-              <a href="#!">Travel Blog</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">About</button>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Contact</button>
-          </div>
-        </div>
-      </div>
-
+      <Navigation />
       <div className="bio-section">
         <h2>Explore.Dream.Discover</h2>
         <p>Hello! I'm a travel enthusiast who loves exploring new places and cultures. I believe that travel is not just about visiting new places but about experiencing life in different ways.</p>
