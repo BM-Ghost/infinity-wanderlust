622b9e0306737597eabb47e7f8b452a65d5c73f2
Meshack Bwire <b.meshack@outlook.com>
2025-02-15 13:40:49 +0300

    backend port 10001

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { exec } = require('child_process');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10001; // Use 10001 for the backend
 
 app.use(cors());
 
